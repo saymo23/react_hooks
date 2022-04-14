a1189d01370e16acfb2c7d2cbddb59ec0beda868
Daniel_S <daniel@mexiconow.mx>
2022-04-14 11:35:36 -0600

    107 useState

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CounterApp from './components/01-useState/CounterApp';
 import HookApp from './HookApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HookApp />
+    <CounterApp />
 );
 
