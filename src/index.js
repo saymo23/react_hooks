55c3f7ff3cc0b6d2b551c1f444652e2fdb33ee85
Daniel_S <daniel@mexiconow.mx>
2022-04-18 13:35:30 -0600

    112 Custom Hook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { FormWithCustomHooks } from './components/02-userEfect/FormwithCustomHook';
 //import CounterApp from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-userEfect/SimpleForm';
 //import HookApp from './HookApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CounterWithCustomHook />
+    <FormWithCustomHooks />
 );
 
