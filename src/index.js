5d3c3b6c989224f931204e5bf2e9a4f51889d285
Daniel_S <daniel@mexiconow.mx>
2022-04-26 16:27:28 -0600

    113

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { FormWithCustomHooks } from './components/02-userEfect/FormwithCustomHook';
 //import CounterApp from './components/01-useState/CounterApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-userEfect/SimpleForm';
+//import { SimpleForm } from './components/02-userEfect/SimpleForm';
+import { MultipleCustomHooks } from './components/examples/MultipleCustomHooks';
 //import HookApp from './HookApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <FormWithCustomHooks />
+    <MultipleCustomHooks />
 );
 
